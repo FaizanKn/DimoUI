89d6ba71e798580dab605e7a8220d5b2437c5cba
Aashu <dagar.aashu004@gmail.com>
2020-02-17 16:32:16 +0530

    <Aashu, Faizan> removed bootstrap dependency

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginModule } from './login/login.module';
 import { LoginComponent } from './login/components/login/login.component';
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap"
 
 
 const routes: Routes = [{
@@ -11,7 +10,7 @@ const routes: Routes = [{
 }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), LoginModule, NgbModule],
+  imports: [RouterModule.forRoot(routes), LoginModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
