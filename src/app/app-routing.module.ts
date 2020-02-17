d21b467ead69d83f628b9c4240ea81b6a266bfd6
Aashu <dagar.aashu004@gmail.com>
2020-02-17 11:41:52 +0530

    <aashu,faizaan> added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginModule } from './login/login.module';
+import { LoginComponent } from './login/components/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "",
+  component: LoginComponent
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), LoginModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
