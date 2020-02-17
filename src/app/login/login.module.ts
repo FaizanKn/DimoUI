789e915c9d359ea94ac6fee529ea07bfd7d651f1
Aashu <aashu.rani@thoughtworks.com>
2020-02-17 11:41:52 +0530

    <aashu,faizaan> added components

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [RegisterUserComponent, LoginComponent],
   imports: [
     CommonModule,
     LoginRoutingModule
