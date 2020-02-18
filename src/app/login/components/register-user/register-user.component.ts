80c1d4a2231d47adc1f0394824f2ff3c916afc08
Mukesh Ranjan <mukesh.ranjan@thoughtworks.com>
2020-02-18 14:40:26 +0530

    added prefernce lsit

--- src/app/login/components/register-user/register-user.component.ts
+++ src/app/login/components/register-user/register-user.component.ts
@@ -12,10 +12,17 @@ import { User } from '../../model/user.model';
 export class RegisterUserComponent implements OnInit {
 
   public form: FormGroup;
+  public languagePreference: Array<any> = [];
+  public zenrePreference: Array<any> = [];
+  public productionHousePreference: Array<any> = [];
+  public dropdownSettings: any = {};
+ 
+
   constructor(private formBuilder: FormBuilder, private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
     this.initForm();
+    this.getLanguageList();
   }
 
   public initForm(){
@@ -23,7 +30,10 @@ export class RegisterUserComponent implements OnInit {
        name: ['', [Validators.required]],
        email: ['', [Validators.required, Validators.email]],
        password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]],
-       confirmPassword: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]]
+       confirmPassword: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]],
+       languagePreference: [],
+       production: [],
+       genre: []
      });
      this.validateConfirmPassword();     
   }
@@ -57,5 +67,44 @@ export class RegisterUserComponent implements OnInit {
   public redirectToLogin(){
     this.router.navigate(['']);
   }
+
+ public getLanguageList(){
+   this.languagePreference = [
+    {"id":1,"itemName":"India"},
+    {"id":2,"itemName":"Singapore"},
+    {"id":3,"itemName":"Australia"},
+    {"id":4,"itemName":"Canada"},
+    {"id":5,"itemName":"South Korea"},
+    {"id":6,"itemName":"Germany"},
+    {"id":7,"itemName":"France"},
+    {"id":8,"itemName":"Russia"},
+    {"id":9,"itemName":"Italy"},
+    {"id":10,"itemName":"Sweden"}
+  ];
+ }
+
+ public getZenreList(){
+   
+}
+
+public getProductionList(){
+   
+}
+
+public setDropDownSetting(textValue: string){
+  return { 
+    singleSelection: false, 
+    text: textValue,
+    selectAllText:'Select All',
+    unSelectAllText:'UnSelect All',
+    lazyLoading : true,
+    enableSearchFilter: true,
+    classes:"myclass custom-class"
+  };        
+}
+
+
+
+
 
 }
