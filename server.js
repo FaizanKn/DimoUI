ec68eedb218a41e4f5ff23d224c5287045aaa2a3
Faizan Khan <faizan.khan@thoughtworks.com>
2020-02-18 10:47:19 +0530

    <Faizan>: Changes for Heroku deployment

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const path = require('path');
 
 const app = express();
 
+const basePath = "https://dimoapi.herokuapp.com";
+
+ 
+const proxy = require('http-proxy-middleware')
+var apiProxy = proxy('/api', {target: basePath});
+app.use(apiProxy);
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/dimoUI'));
 
@@ -13,4 +20,4 @@ res.sendFile(path.join(__dirname+'/dist/dimoUI/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8090,() => console.log("Server started"));
