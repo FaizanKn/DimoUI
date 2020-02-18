7ce1427ef6292317f8e1868f4ff64df305ddbd62
Mukesh Ranjan <mukesh.ranjan@thoughtworks.com>
2020-02-18 11:58:42 +0530

    Reguster validation done

--- src/app/login/components/register-user/register-user.component.ts
+++ src/app/login/components/register-user/register-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-register-user',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterUserComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup;
+  constructor(private formBuilder: FormBuilder, private userService: UserService) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  public initForm(){
+     this.form = this.formBuilder.group({
+       name: ['', [Validators.required]],
+       email: ['', [Validators.required, Validators.email]],
+       password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]],
+     });     
+  }
+
+  public submit(){
+    console.log(this.form.value);
+    this.userService.doRegister(this.form.value).subscribe((resp)=>{
+           console.log(resp);
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
