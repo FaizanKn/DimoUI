e349e5e7a27bf4313c4da64c4ee3bc20e2489123
Aashu <dagar.aashu004@gmail.com>
2020-02-18 10:06:47 +0530

    <aashu> register user html

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "register",
+  component: RegisterUserComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
