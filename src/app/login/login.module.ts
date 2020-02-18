7a143b28123aee4b7650912527be06969f69862a
Mukesh Ranjan <mukesh.ranjan@thoughtworks.com>
2020-02-18 10:36:01 +0530

    created auth service for login/signup

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { RegisterUserComponent } from './components/register-user/register-user.component';
 import { LoginComponent } from './components/login/login.component';
+import { HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
   declarations: [RegisterUserComponent, LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    HttpClientModule
+
   ]
 })
 export class LoginModule { }
