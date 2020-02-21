328992dc2f073f64002350028d41f2708731ed79
Faizan Khan <faizan.khan@thoughtworks.com>
2020-02-21 10:01:18 +0530

    <Faizan>: Added page not found. Made dahsboard responsive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 
 @NgModule({
-  declarations: [NavigationBarComponent],
+  declarations: [NavigationBarComponent, PageNotFoundComponent],
   imports: [
     CommonModule
   ],
